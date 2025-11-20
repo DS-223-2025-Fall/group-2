9480639801087382a13bf6e4da8d7e7092426bf7
VaheSargsyan004 <vahesargsyan387@mail.ru>
2025-11-20 21:48:20 +0400

    chage project data population logic

---
+++ BookFinder/etl/database/models.py
@@ -0,0 +1,24 @@
+from loguru import logger
+
+
+from sqlalchemy import create_engine,Column,Integer,String,Float, DATE, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from datetime import datetime
+from Database.database import Base, engine
+
+# More tables will be added as we gather data for them
+
+class Book(Base):
+    __tablename__ = "book"
+
+    isbn = Column(String, primary_key=True)
+    title = Column(Text)
+    author = Column(Text)
+    genre = Column(Text)
+    language = Column(Text)
+    data_source = Column(Text)
+    description = Column(Text)
+# Define the Sales table
+
+Base.metadata.create_all(engine)
